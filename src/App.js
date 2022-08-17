57e16c9646a70b645746375dece8b623daeb0455
Joe Herrera <joefooser@gmail.com>
2022-08-17 14:47:39 -0500

    Initial ExpenseForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import ExpenseList from "./components/Expenses/ExpenseList";
 
 const App = () => {
@@ -33,7 +35,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <ExpenseList expenses={expenses} />
     </div>
   );
